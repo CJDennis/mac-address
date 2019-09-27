985b21b2a75676ef24610ba94fc016edf3d7feb4
Daniel Klein <danielklein@airpost.net>
2019-09-27 16:29:15 +1000

    Pass the test

--- src/CjDennis/MacAddress/MacAddress.php
+++ src/CjDennis/MacAddress/MacAddress.php
@@ -65,7 +65,7 @@ class MacAddress {
 
   public static function is_valid($mac_address) {
     $is_valid = false;
-    if (preg_match('/\A[\da-f]{2}(?:-[\da-f]{2}){5}\z/i', $mac_address)) {
+    if (preg_match('/\A\s*[\da-f]{2}(?:\W*[\da-f]{2}){5}\s*\z/i', $mac_address)) {
       $is_valid = true;
     }
     return $is_valid;
