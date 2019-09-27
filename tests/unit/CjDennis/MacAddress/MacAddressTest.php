d62f57f199dfd646658420f44c05e85d28aec026
Daniel Klein <danielklein@airpost.net>
2019-09-27 20:26:49 +1000

    Add a test to get a MAC address in hex format from a binary string

--- tests/unit/CjDennis/MacAddress/MacAddressTest.php
+++ tests/unit/CjDennis/MacAddress/MacAddressTest.php
@@ -135,4 +135,12 @@ OUTPUT
   public function testShouldVerifyAMacAddressIsNotUnicast() {
     $this->assertFalse(MacAddressSeam::is_unicast('F1-FF-FF-FF-FF-FF'));
   }
+
+  public function testShouldOutputAHexMacAddressFromABinaryString() {
+    $this->assertSame('123AD64BC5EF', MacAddress::hex("\x12\x3A\xD6\x4B\xC5\xEF"));
+  }
+  //
+  //public function testShouldOutputAHexMacAddressFromABinaryString() {
+  //  $this->assertSame('12-3A-D6-4B-C5-EF', MacAddress::hex("\x12\x3A\xD6\x4B\xC5\xEF"));
+  //}
 }
