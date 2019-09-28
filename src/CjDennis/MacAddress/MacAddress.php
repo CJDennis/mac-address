96925d467bca615104c5646b782ea2a5e8c1c98e
Daniel Klein <danielklein@airpost.net>
2019-09-28 14:46:47 +1000

    Pass the test

--- src/CjDennis/MacAddress/MacAddress.php
+++ src/CjDennis/MacAddress/MacAddress.php
@@ -113,6 +113,7 @@ class MacAddress {
   }
 
   public static function format($mac_address, $delimiter = '-') {
+    $mac_address = static::hex($mac_address);
     static::validate_delimiter($delimiter);
     return join($delimiter, str_split(strtoupper($mac_address), 2));
   }
